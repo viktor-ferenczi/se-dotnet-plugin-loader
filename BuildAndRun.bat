f6460e75cfd969357713c94ec6fc33fe7907e579
Viktor Ferenczi <viktor@ferenczi.eu>
2024-11-02 18:12:24 +0100

    Fixes

--- BuildAndRun.bat
+++
@@ -1,5 +0,0 @@
-set SolutionDir=%cd%
-dotnet dotnet build --configuration Release
-if %ERRORLEVEL% NEQ 0 goto :end
-dotnet run --configuration Release --project SpaceEngineersLauncher -- -skipintro
-:end
